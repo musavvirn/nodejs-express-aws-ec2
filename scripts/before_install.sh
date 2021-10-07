1f8115b7615e38949f8949feac78f6500638e3c4
musavvir <musavvir.noor@gmail.com>
2021-10-07 16:09:31 -0700

    sdsds

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 #download node and npm
+
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
+
+
+
 nvm install node
 
 #create our working directory if it doesnt exist
